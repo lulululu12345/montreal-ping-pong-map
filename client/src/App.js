9a1f0e66b6efa924fc8fbe89fd53ac96fca0c1e3
Luke Steinmann <luke.steinmann@gmail.com>
2022-05-28 14:02:43 -0400

    install google maps react wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Map from './components/Map'
 function App() {
   return (
     <div className="App">
       <h2>Montreal Ping Pong Map</h2>
+      <Map/>
     </div>
   )
 }
---
+++ client/src/components/Map.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import { useState } from 'react'
+
