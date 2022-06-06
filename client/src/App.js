d0efa73a6dc6a97d9410c442591f5ae7ca90c77f
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-06 13:59:45 -0400

    Add react-google-maps/api and build basic Map component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import { useMemo } from 'react'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 import Map from './components/Map'
+import './App.css';
+
+
 function App() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyDhzpBCgy9vMfzQLCo5MtxtkgkWt-6-TRs'
+  })
+
+  if (!isLoaded) return <div>Loading...</div>
+
   return (
     <div className="App">
       <h2>Montreal Ping Pong Map</h2>
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { Wrapper, Status } from '@googlemaps/react-wrapper'
-import { useState } from 'react'
+import { useMemo } from 'react'
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 
+const Map = () => {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), [])
+
+  return (
+    <GoogleMap 
+      zoom={10}  
+      center={center} 
+      mapContainerClassName='map-container'
+    >
+      <Marker position={center} />
+    </GoogleMap>
+  )
+}
+
+export default Map
