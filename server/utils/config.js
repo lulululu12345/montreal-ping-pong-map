f7682f4d97fc5a64742cae45657ebda31475e237
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-08 15:10:20 -0400

    Add MAP_ID to config.js

--- server/utils/config.js
+++ server/utils/config.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.GOOGLE_MAPS_API_KEY
+const MAP_ID = process.env.GOOGLE_MAPS_MAP_ID
 
 
 module.exports = {
   MONGODB_URI,
   PORT,
-  API_KEY
+  API_KEY,
+  MAP_ID
 }
