3efe397aaa37ba5793b79e67061e0cda3e44ebc1
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-08 15:01:32 -0400

    Add mongoose.connect to app.js

--- server/app.js
+++ server/app.js
@@ -6,15 +6,15 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-// logger.info(`Connecting to ${config.MONGODB_URI}`)
+logger.info(`Connecting to ${config.MONGODB_URI}`)
 
-// mongoose.connect(config.MONGODB_URI)
-//   .then(() => {
-//     logger.info('connected to MongoDB')
-//   })
-//   .catch(error => {
-//     logger.error(`error connecting to MongoDB ${error.message}`)
-//   })
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error(`error connecting to MongoDB ${error.message}`)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
