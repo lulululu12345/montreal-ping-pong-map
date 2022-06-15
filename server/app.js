64bd622b64ca2d8a8a88bcb116ba921373b4c65b
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-15 10:53:37 -0400

    Add buildPath to app.js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,6 +7,7 @@ const middleware = require('./utils/middleware')
 const tablesRouter = require('./controllers/tables')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const buildPath = path.join(__dirname, '..', 'client', 'build')
 
 logger.info(`Connecting to ${config.MONGODB_URI}`)
 
@@ -18,7 +20,7 @@ mongoose.connect(config.MONGODB_URI)
   })
 
 app.use(cors())
-app.use(express.static('build'))
+app.use(express.static(buildPath))
 
 app.use(express.json())
 app.use(middleware.requestLogger)
