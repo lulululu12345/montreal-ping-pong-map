49bf7a453bdeeb4f69061920cd36d941cf29d026
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-15 09:16:20 -0400

    Add environment variables to React project root directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-// import { useState } from 'react'
+import React from 'react'
 import { useLoadScript } from '@react-google-maps/api'
 import Map from './components/Map'
 import './App.css';
 
-
 function App() {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyDhzpBCgy9vMfzQLCo5MtxtkgkWt-6-TRs',
-    mapIds: ['d36d858f2192af61']
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    mapIds: [process.env.REACT_APP_MAP_ID]
+    // googleMapsApiKey: 'AIzaSyDhzpBCgy9vMfzQLCo5MtxtkgkWt-6-TRs',
+    // mapIds: ['d36d858f2192af61']
   })
 
   if (!isLoaded) return <div>Loading...</div>
-
   return (
     <div className="App">
       <h1>Montreal Ping Pong Map</h1>
