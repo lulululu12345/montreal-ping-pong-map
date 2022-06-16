e92abce08d67cb22f18c89491ce55ff61e23fd2f
Luke Steinmann <luke.steinmann@gmail.com>
2022-06-16 09:54:29 -0400

    Add images for custom markers and marker info windows

--- client/src/components/TableMarker.js
+++ client/src/components/TableMarker.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Marker, InfoWindow } from '@react-google-maps/api'
+import table from '../assets/images/table.png'
+import paddle from '../assets/images/paddle.png'
 
 const TableMarker = ({ key, title, position, numberOfTables, location, payToPlay, description, verified }) => {
   const [isOpen, setIsOpen] = React.useState(false)
@@ -12,11 +14,13 @@ const TableMarker = ({ key, title, position, numberOfTables, location, payToPlay
     <>
       <Marker
         title={title}
+        icon={paddle}
         position={position}
         onClick={clickMarker}
       >
         {isOpen && <InfoWindow position={position}>
           <div className='info-window'>
+            <img src={table} alt='Ping pong table graphic'/>
             {numberOfTables < 2
               ? <p className='info-line'>{`${numberOfTables} ${location} table`}</p>
               : <p className='info-line'>{`${numberOfTables} ${location} tables`}</p>
